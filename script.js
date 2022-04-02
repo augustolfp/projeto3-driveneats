baac05f50dbf173c9616999fab7dcd10059c0dce
augustolfp <augustolfp@gmail.com>
2022-04-02 19:55:12 -0300

    Implementação do botão de checkout

--- script.js
+++ script.js
@@ -5,4 +5,24 @@ function SelecionarProduto(ElementoClicado) {
         ItemJaSelecionado.classList.remove("ItemSelecionado");
     }
     ElementoClicado.classList.add("ItemSelecionado")
+    EstadoSelecao();
+}
+
+function NumeroDeProdutosSelecionados() {
+    let num = document.querySelectorAll(".ItemSelecionado").length;
+    return num;
+}
+
+function EstadoSelecao() {
+    if(NumeroDeProdutosSelecionados()===3) {
+        let BotaoCinza = document.querySelector(".SelecioneOs3Itens");
+        let BotaoVerde = document.querySelector(".FecharPedido");
+        BotaoCinza.classList.add("Escondido");
+        BotaoVerde.classList.remove("Escondido");
+    }
+}
+
+function Checkout() {
+    let TransparentBackground = document.querySelector(".BlurBackground");
+    TransparentBackground.classList.remove("Escondido");
 }
