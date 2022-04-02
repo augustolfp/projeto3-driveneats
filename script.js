3d3991a1bd9dfb3f5116d58c19afc73702cf7a8c
augustolfp <augustolfp@gmail.com>
2022-04-02 17:47:59 -0300

    Implementação da interface de seleção de produtos

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+function SelecionarProduto(ElementoClicado) {
+    let ElementoPai = ElementoClicado.parentNode;
+    let ItemJaSelecionado = ElementoPai.querySelector(".ItemSelecionado");
+    if(ItemJaSelecionado!==null) {
+        ItemJaSelecionado.classList.remove("ItemSelecionado");
+    }
+    ElementoClicado.classList.add("ItemSelecionado")
+}
