46d1fdfaaf51011d215a7daa9a2347ac9f69d1e8
augustolfp <augustolfp@gmail.com>
2023-02-27 19:01:14 -0300

    refactor: Move product lists to separate file

---
+++ data.js
@@ -0,0 +1,62 @@
+export const dishes = [
+    {
+        nome: "Frango Yin Yang",
+        imagem: "frango.png",
+        descricao: "Um pouco de batata, um pouco de salada",
+        preco: 14.9,
+    },
+    {
+        nome: "Macarrão médio",
+        imagem: "frango.png",
+        descricao: "Massa fresquinha",
+        preco: 28.5,
+    },
+    {
+        nome: "Yakissoba tradicional",
+        imagem: "frango.png",
+        descricao: "Receita familiar!",
+        preco: 19.9,
+    },
+];
+
+export const beverages = [
+    {
+        nome: "Coquinha gelada",
+        imagem: "coca.png",
+        descricao: "Lata 350ml",
+        preco: 4.9,
+    },
+    {
+        nome: "Cervejinha",
+        imagem: "coca.png",
+        descricao: "Lata 350ml",
+        preco: 5.5,
+    },
+    {
+        nome: "Água com gás",
+        imagem: "coca.png",
+        descricao: "Garrafa 500ml",
+        preco: 2.9,
+    },
+];
+
+export const desserts = [
+    {
+        nome: "Pudim",
+        imagem: "pudim.png",
+        descricao: "Apenas pudim",
+        preco: 7.9,
+    },
+    {
+        nome: "Gelatina",
+        imagem: "pudim.png",
+        descricao: "Feita com muito carinho",
+        preco: 5.8,
+    },
+    {
+        nome: "Salada de frutas",
+        imagem: "pudim.png",
+        descricao: "Para os Fitness de plantão",
+        preco: 9.9,
+    },
+];
--- script.js
+++ script.js
@@ -1,73 +1,12 @@
-import { sayHello } from "./beverage.js";
-
-sayHello();
+import {
+    dishes as pratos,
+    beverages as bebidas,
+    desserts as sobremesas,
+} from "./data.js";
 
 let pratoSelecionado = null;
 let bebidaSelecionada = null;
 let sobremesaSelecionada = null;
-
-const pratos = [
-    {
-        nome: "Frango Yin Yang",
-        imagem: "frango.png",
-        descricao: "Um pouco de batata, um pouco de salada",
-        preco: 14.9,
-    },
-    {
-        nome: "Macarrão médio",
-        imagem: "frango.png",
-        descricao: "Massa fresquinha",
-        preco: 28.5,
-    },
-    {
-        nome: "Yakissoba tradicional",
-        imagem: "frango.png",
-        descricao: "Receita familiar!",
-        preco: 19.9,
-    },
-];
-
-const bebidas = [
-    {
-        nome: "Coquinha gelada",
-        imagem: "coca.png",
-        descricao: "Lata 350ml",
-        preco: 4.9,
-    },
-    {
-        nome: "Cervejinha",
-        imagem: "coca.png",
-        descricao: "Lata 350ml",
-        preco: 5.5,
-    },
-    {
-        nome: "Água com gás",
-        imagem: "coca.png",
-        descricao: "Garrafa 500ml",
-        preco: 2.9,
-    },
-];
-
-const sobremesas = [
-    {
-        nome: "Pudim",
-        imagem: "pudim.png",
-        descricao: "Apenas pudim",
-        preco: 7.9,
-    },
-    {
-        nome: "Gelatina",
-        imagem: "pudim.png",
-        descricao: "Feita com muito carinho",
-        preco: 5.8,
-    },
-    {
-        nome: "Salada de frutas",
-        imagem: "pudim.png",
-        descricao: "Para os Fitness de plantão",
-        preco: 9.9,
-    },
-];
 
 const pratosContainer = document.querySelector(".Pratos");
 const bebidasContainer = document.querySelector(".Bebidas");
